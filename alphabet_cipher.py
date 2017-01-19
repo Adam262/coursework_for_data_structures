3db2031b1ed3b3b9e2dbafda079c81d6ca336ec5
Adam262 <abarcan@gmail.com>
2017-01-19 00:00:52 -0500

    Get decipher working

--- alphabet_cipher.py
+++ alphabet_cipher.py
@@ -16,6 +16,19 @@ class AlphabetCipher:
 
     return ''.join(map(self.__decode_single_char, list(cycled_keyword), list(encrypted_message)))
 
+  def deciper(self, message, encrypted_message):
+    if len(message) == len(encrypted_message):
+      cycled_keyword = ''.join(map(self.__decipher_single_char, message, encrypted_message))
+      base = ""
+
+      for char in cycled_keyword:
+        base += char
+        if self.__cycle(base, len(cycled_keyword)) == cycled_keyword:
+          return base
+
+  def __decipher_single_char(self, message_char, encrypted_message_char):
+    return [key for key in self.grid if self.grid[key].index(encrypted_message_char) == self.alphabet.index(message_char)][0]
+
   def __make_grid(self, memo, char):
     idx = self.alphabet.index(char)
     memo[char] = self.alphabet[idx:] + self.alphabet[:idx]
@@ -44,7 +57,9 @@ class AlphabetCipher:
 cipher = AlphabetCipher(alphabet="abc", keyword="bc")
 encoded_message = cipher.encode(message='cabba')
 print('encoded message: ' + encoded_message, 'decoded message: ' + cipher.decode(encoded_message))
+print('cipher', cipher.deciper('cabba', encoded_message))
 
-cipher = AlphabetCipher(alphabet="abcdefghijklmnopqrstuvwxyz", keyword="scones")
-encoded_message = cipher.encode(message='meetmebythetree')
+cipher = AlphabetCipher(alphabet="abcdefghijklmnopqrstuvwxyz", keyword="vigilance")
+encoded_message = cipher.encode(message='meetmeontuesdayeveningatseven')
 print('encoded message: ' + encoded_message, 'decoded message: ' + cipher.decode(encoded_message))
+print('cipher', cipher.deciper('meetmeontuesdayeveningatseven', encoded_message))
